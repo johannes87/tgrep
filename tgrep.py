d6a219a0727d452a192d9a4a2d77a3258361c1d2
Johannes Bittner <johannes.bittner@gmail.com>
2011-02-19 12:50:23 +0100

    edge case (time < min(all timestamps)) works now

--- tgrep.py
+++ tgrep.py
@@ -23,31 +23,36 @@ def get_next_time_tuple(fd, pos, file_size):
     return (None, None)
 
 
-def binary_search(fd, search_time_tuple, start_byte, end_byte, file_size):
+def binary_search(fd, search_time_tuple, start_byte, end_byte, file_size, depth=0):
     center_byte = start_byte + (end_byte - start_byte) / 2 
     (found_time_tuple, last_found_byte) = get_next_time_tuple(fd, center_byte, file_size)
     
+
     if found_time_tuple == None:
         print "no timestamp found"
         return
     
     if last_found_byte > end_byte:
-        print "found nearest timestamp: {0}".format(time.strftime(TS_FORMAT, found_time_tuple))
-        return
+        if center_byte <= start_byte:
+            print "found nearest timestamp: {0}".format(time.strftime(TS_FORMAT, found_time_tuple))
+            return
+        else:
+            binary_search(fd, search_time_tuple, start_byte, center_byte - 1, file_size, depth + 1)
 
-    if found_time_tuple == search_time_tuple:
-        print "found timestamp: {0}".format(time.strftime(TS_FORMAT, found_time_tuple))
-    elif search_time_tuple > found_time_tuple:
-        binary_search(fd, search_time_tuple, last_found_byte + 1, end_byte, file_size)
     else:
-        binary_search(fd, search_time_tuple, start_byte, center_byte - 1, file_size)
+        if found_time_tuple == search_time_tuple:
+            print "found timestamp: {0}".format(time.strftime(TS_FORMAT, found_time_tuple))
+        elif search_time_tuple > found_time_tuple:
+            binary_search(fd, search_time_tuple, last_found_byte + 1, end_byte, file_size, depth + 1)
+        else:
+            binary_search(fd, search_time_tuple, start_byte, center_byte - 1, file_size, depth + 1)
 
 
 def main():
     fd = os.open(LOGFILE, os.O_RDONLY)
     file_size = os.path.getsize(LOGFILE)
 
-    search_time_tuple = time.strptime("01/Aug/1983:03:41:09", "%d/%b/%Y:%H:%M:%S")
+    search_time_tuple = time.strptime("21/Jan/1970:18:56:40", "%d/%b/%Y:%H:%M:%S")
 
     binary_search(fd, search_time_tuple, 0, file_size - 1, file_size)
     os.close(fd)
