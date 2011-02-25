1705ba2ae88188e4f99462039673cf3533a8aeb8
Johannes Bittner <johannes.bittner@gmail.com>
2011-02-25 21:42:47 +0100

    minor cleanup

--- tgrep.py
+++ tgrep.py
@@ -3,19 +3,18 @@ import os
 import time
 
 class BinaryLogSearch(object):
-    def __init__(self, log_path, timestamp_format, timestamp_length, search_timestamp, line_delimiter="\n"):
+    def __init__(self, log_path, timestamp_format, timestamp_length, search_timestamp):
         self.log_size = os.path.getsize(log_path)
         self.log_fd = os.open(log_path, os.O_RDONLY)
         self.search_time_tuple = time.strptime(search_timestamp, timestamp_format)
         self.timestamp_format = timestamp_format
         self.timestamp_length = timestamp_length
-        self.line_delimiter = line_delimiter
     
     def start_search(self):
         self.__found_pos = None
         self.__search(0, self.log_size - 1)
 
-    def show_found(self, lines_before=0, lines_after=0):
+    def show_found(self, lines_before=0, lines_after=0, line_delimiter="\n"):
         if self.__found_pos == None:
             print "nothing found"
             return
@@ -24,23 +23,23 @@ class BinaryLogSearch(object):
         found_lines_before = 0
         while cur_pos >= 0 and found_lines_before <= lines_before:
             os.lseek(self.log_fd, cur_pos, os.SEEK_SET)
-            found = os.read(self.log_fd, len(self.line_delimiter))
-            if found == self.line_delimiter:
+            found = os.read(self.log_fd, len(line_delimiter))
+            if found == line_delimiter:
                 found_lines_before += 1
             cur_pos -= 1
         
-        output_start = cur_pos + 1 + len(self.line_delimiter)
+        output_start = cur_pos + 1 + len(line_delimiter)
         
         cur_pos = self.__found_pos + 1
         found_lines_after = 0
         while cur_pos < self.log_size and found_lines_after <= lines_after:
             os.lseek(self.log_fd, cur_pos, os.SEEK_SET)
-            found = os.read(self.log_fd, len(self.line_delimiter))
-            if found == self.line_delimiter:
+            found = os.read(self.log_fd, len(line_delimiter))
+            if found == line_delimiter:
                 found_lines_after += 1
             cur_pos += 1
         
-        output_length = cur_pos - 1 + len(self.line_delimiter) - output_start
+        output_length = cur_pos - 1 + len(line_delimiter) - output_start
         os.lseek(self.log_fd, output_start, os.SEEK_SET)
         output = os.read(self.log_fd, output_length)
         
@@ -79,7 +78,7 @@ class BinaryLogSearch(object):
         
         if last_read_pos > end_pos:
             if center_pos <= start_pos:
-                # found nearest timestamp
+                # found next timestamp
                 self.__found_pos = last_read_pos
                 return
             else:
@@ -95,7 +94,7 @@ class BinaryLogSearch(object):
                 self.__search(start_pos, center_pos - 1)
 
 def main():
-    with BinaryLogSearch("log.txt", "%d/%b/%Y:%H:%M:%S", 20, "21/Jan/1970:18:56:39") as log_search:
+    with BinaryLogSearch("log.txt", "%d/%b/%Y:%H:%M:%S", 20, "21/Jan/1975:18:56:41") as log_search:
         log_search.start_search()
         log_search.show_found(3, 3)
 
