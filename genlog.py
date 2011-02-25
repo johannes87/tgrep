399c2eb4507185eb3a7d7717e82390d1347073ef
Johannes Bittner <johannes.bittner@gmail.com>
2011-02-25 21:47:44 +0100

    gen random logs

--- genlog.py
+++ genlog.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 from time import strftime, gmtime
+import random
 
-template='127.0.0.1 - frank [%d/%b/%Y:%H:%M:%S -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326'
+template='127.0.0.1 - frank [%d/%b/%Y:%H:%M:%S -0700] "GET {0} /apache_pb.gif HTTP/1.0" 200 2326'
 
 t_secs = 0
 while True:
-    print strftime(template, gmtime(t_secs))
+    print strftime(template, gmtime(t_secs)).format(random.randint(0,30) * ' ')
     t_secs += 10
